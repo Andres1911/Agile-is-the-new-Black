7757fb8f2c6be8335679c7bd165fd9463d9aec28
Samy Harras <samy.harras@mail.mcgill.ca>
2026-02-24 20:57:32 -0500

    Update expenses.py

--- backend/app/api/expenses.py
+++ backend/app/api/expenses.py
@@ -12,7 +12,7 @@ from app.models.models import (
     User,
     VoteStatus,
 )
-from app.schemas.schemas import ConfirmPaymentRequest, ExpenseCreate, OutstandingExpense, RespondExpenseShareRequest
+from app.schemas.schemas import ConfirmPaymentRequest, ExpenseCreate, RespondExpenseShareRequest
 
 
 router = APIRouter()
